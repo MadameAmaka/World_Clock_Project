68263b3b8fc51da15c0b535eda06ef94ff911ece
Amaka Onwuli <AOnwuli@st-andrews.rochester.sch.uk>
2024-09-14 15:09:44 +0100

    added a city timezone on html and a current location functionality on js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ chicagoTimeElement.innerHTML = chicagoTime.format(
 }
 function updateCity(event){
     let cityTimeZone = event.target.value;
+    if (cityTimeZone==="current"){
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone)
     let citiesElement = document.querySelector("#cities");
