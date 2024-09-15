3b4e56a11511bcf6e5d7cf0d31a5d301c5eac85d
Amaka Onwuli <AOnwuli@st-andrews.rochester.sch.uk>
2024-09-15 18:28:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ function updateCity(event){
         <small>${cityTime.format("A")}</small>
         </div>
       </div>
-      <a href="index.html">Reset</a>`;
+      <a href="/">Reset</a>`;
 }
 updateTime();
 setInterval(updateTime, 1000);
